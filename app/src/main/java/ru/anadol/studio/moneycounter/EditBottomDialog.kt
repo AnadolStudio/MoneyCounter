5049d72412d181179944ca466f8f9dbc6c2d9898
Bogdan Terehov <bterehov@licard.com>
2023-07-07 12:55:39 +0300

    update core base

--- app/src/main/java/ru/anadol/studio/moneycounter/EditBottomDialog.kt
+++ app/src/main/java/ru/anadol/studio/moneycounter/EditBottomDialog.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
-import com.anadolstudio.core.dialogs.CoreBottomSheetDialog
+import com.anadolstudio.core.dialogs.bottom_sheet.CoreBottomSheetDialog
 import com.anadolstudio.core.viewbinding.viewBinding
 import ru.anadol.studio.moneycounter.databinding.FragmentBottomDialogEditBinding
 
@@ -12,10 +12,6 @@ class EditBottomDialog : CoreBottomSheetDialog(R.layout.fragment_bottom_dialog_e
 
     override fun getDialogTag(): String {
         TODO("Not yet implemented")
-    }
-
-    override fun setStyle() {
-        setStyle(DialogFragment.STYLE_NORMAL, com.anadolstudio.core.R.style.RoundCornerBottomSheetTheme)
     }
 
     private val binding by viewBinding(FragmentBottomDialogEditBinding::bind)
--- app/src/main/java/ru/anadol/studio/moneycounter/FirstFragment.kt
+++ app/src/main/java/ru/anadol/studio/moneycounter/FirstFragment.kt
@@ -1,22 +1,14 @@
 package ru.anadol.studio.moneycounter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.anadolstudio.core.common_util.throttleClick
-import com.anadolstudio.core.fragment.CoreBaseFragment
+import com.anadolstudio.core.fragment.CoreContentBaseFragment
 import com.anadolstudio.core.viewbinding.viewBinding
-import com.google.android.material.snackbar.Snackbar
 import ru.anadol.studio.moneycounter.databinding.FragmentFirstBinding
 
-class FirstFragment : CoreBaseFragment<Unit>(R.layout.fragment_first) {
-
-    override fun render(state: Unit) {
-        TODO("Not yet implemented")
-    }
+class FirstFragment : CoreContentBaseFragment<Unit>(R.layout.fragment_first) {
 
     private val binding: FragmentFirstBinding by viewBinding { FragmentFirstBinding.bind(requireView()) }
 
